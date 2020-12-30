a5514a1521451f14fca7ab1aa14c54f6ebb75820
Jicking Bebiro <jicking.dev@gmail.com>
2020-12-30 16:31:52 +0800

    add  form

--- src/pages/TodoEdit.tsx
+++ src/pages/TodoEdit.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import useAxios from "axios-hooks";
+import React, { Fragment } from "react";
+import { Button, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function TodoEdit() {
-  let { id } = useParams();
+  const { id } = useParams();
+  const [{ data: getData, loading: getLoading, error: getError }] = useAxios(
+    `${process.env.REACT_APP_API_ROOT_URL}/todos/${id}`
+  );
+  const { register, handleSubmit } = useForm(getData);
+  const onSubmit = (data) => console.log(data);
 
-  return <h1>Edit Todo: {id}</h1>;
+  if (getLoading) return <p>Loading...</p>;
+  if (getError) return <p>Error!</p>;
+
+  return (
+    <Fragment>
+      <h1>Edit Todo</h1>
+      <pre>{JSON.stringify(getData, null, 2)}</pre>
+
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group controlId="formDescription">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Description"
+            name="description"
+            defaultValue={getData.description}
+            ref={register}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formCheckbox">
+          <Form.Check
+            type="checkbox"
+            label="Done?"
+            name="isDone"
+            defaultChecked={getData.isClosed}
+            ref={register}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Fragment>
+  );
 }
 
 export default TodoEdit;
